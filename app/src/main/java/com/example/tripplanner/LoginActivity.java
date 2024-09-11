e105a88dfd181a49b30d32a78641738d8fe5a0a4
evavaa <evach0305@gmail.com>
2024-09-11 15:47:23 +1000

    add login UI

---
+++ app/src/main/java/com/example/tripplanner/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.tripplanner;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tripplanner.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_login);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+}
