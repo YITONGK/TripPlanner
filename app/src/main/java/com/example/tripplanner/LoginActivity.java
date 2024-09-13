8bff905ec0c23037577eb8fc44c271694bc7b41e
Kexin (Clare) Chen <108605930+kexincchen@users.noreply.github.com>
2024-09-13 13:34:18 +1000

    added signup function

--- app/src/main/java/com/example/tripplanner/LoginActivity.java
+++ app/src/main/java/com/example/tripplanner/LoginActivity.java
@@ -48,6 +48,15 @@ public class LoginActivity extends AppCompatActivity {
                 // }
             }
         });
+
+        // jump to sign up page
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void signIn(String email, String password) {
--- app/src/main/java/com/example/tripplanner/SignupActivity.java
+++ app/src/main/java/com/example/tripplanner/SignupActivity.java
@@ -6,11 +6,18 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.tripplanner.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignupActivity extends AppCompatActivity {
     private ActivitySignupBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,9 @@ public class SignupActivity extends AppCompatActivity {
 
         binding = ActivitySignupBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         TextView username = (TextView) findViewById(R.id.username);
         TextView email = (TextView) findViewById(R.id.emailAddress);
@@ -40,12 +50,37 @@ public class SignupActivity extends AppCompatActivity {
                     Toast.makeText(SignupActivity.this, "Mismatch password", Toast.LENGTH_SHORT).show();
                 } else {
                     // store user authentication details to database
+
                     Toast.makeText(SignupActivity.this, "Sign up successfully", Toast.LENGTH_SHORT).show();
+                    createAccount(email.getText().toString(), password.getText().toString());
                 }
 
             }
         });
 
 
+    }
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
     }
 }
