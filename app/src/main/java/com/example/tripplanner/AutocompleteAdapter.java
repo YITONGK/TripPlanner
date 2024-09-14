ac6e141a81af980a9e5d21593cdefb49e7efcd79
YITONGK <yitongk@student.unimelb.edu.au>
2024-09-14 13:20:56 +1000

    Place auto complete

---
+++ app/src/main/java/com/example/tripplanner/AutocompleteAdapter.java
@@ -0,0 +1,36 @@
+package com.example.tripplanner;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
+
+import java.util.List;
+
+public class AutocompleteAdapter extends ArrayAdapter<AutocompletePrediction> {
+    public AutocompleteAdapter(Context context, List<AutocompletePrediction> predictions) {
+        super(context, android.R.layout.simple_expandable_list_item_2, android.R.id.text1, predictions);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = super.getView(position, convertView, parent);
+        AutocompletePrediction item = getItem(position);
+
+        TextView textView1 = row.findViewById(android.R.id.text1);
+        TextView textView2 = row.findViewById(android.R.id.text2);
+        Log.d("Place detail", item.toString());
+        textView1.setText(item.getPrimaryText(null));
+        textView2.setText(item.getSecondaryText(null));
+
+        return row;
+    }
+}
--- app/src/main/java/com/example/tripplanner/CreateNewPlanActivity.java
+++ app/src/main/java/com/example/tripplanner/CreateNewPlanActivity.java
@@ -1,44 +1,105 @@
 package com.example.tripplanner;
 
 import android.os.Bundle;
-
-import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CreateNewPlanActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_create_new_plan);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//    }
+import java.util.ArrayList;
+
+public class CreateNewPlanActivity extends AppCompatActivity {
+    private EditText editTextMessage;
+    private ListView listViewAutocomplete;
+    private AutocompleteAdapter adapter;
+    private PlacesClient placesClient;
+    final String apiKey = BuildConfig.PLACES_API_KEY;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (apiKey.equals("")) {
+            Toast.makeText(this, "API key error", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+        }
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_new_plan);
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+        }
+        placesClient = Places.createClient(this);
 
         ImageButton closeButton = findViewById(R.id.closeButton);
         closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Finish the activity and return to the main activity
                 finish();
             }
         });
 
-        // Remaining setup for EditText and Button as earlier
+        editTextMessage = findViewById(R.id.editTextMessage);
+        listViewAutocomplete = findViewById(R.id.listViewAutocomplete);
+
+        adapter = new AutocompleteAdapter(this, new ArrayList<>());
+        listViewAutocomplete.setAdapter(adapter);
+        listViewAutocomplete.setOnItemClickListener((parent, view, position, id) -> {
+            AutocompletePrediction prediction = adapter.getItem(position);
+            editTextMessage.setText(prediction.getFullText(null));
+        });
+
+        editTextMessage.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    performAutocomplete(s.toString());
+                    listViewAutocomplete.setVisibility(View.VISIBLE);
+                } else {
+                    adapter.clear();
+                    adapter.notifyDataSetChanged();
+                    listViewAutocomplete.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private void performAutocomplete(String query) {
+        FindAutocompletePredictionsRequest request = FindAutocompletePredictionsRequest.builder()
+                .setQuery(query)
+                .build();
+
+        placesClient.findAutocompletePredictions(request).addOnSuccessListener(response -> {
+            adapter.clear();
+            adapter.addAll(response.getAutocompletePredictions());
+            adapter.notifyDataSetChanged();
+        }).addOnFailureListener(exception -> {
+            if (exception instanceof ApiException) {
+                ApiException apiException = (ApiException) exception;
+                Toast.makeText(CreateNewPlanActivity.this, "Error fetching autocomplete predictions: " + apiException.getStatusCode(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
