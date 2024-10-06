39e1d052d1a4ff203776585bd2a068e70f7d220a
YITONGK <yitongk@student.unimelb.edu.au>
2024-10-06 16:08:18 +1100

    debug plan name setting

--- app/src/main/java/com/example/tripplanner/entity/ActivityItem.java
+++ app/src/main/java/com/example/tripplanner/entity/ActivityItem.java
@@ -77,6 +77,13 @@ public class ActivityItem {
         return "";
     }
 
+    public String getLocationString() {
+        if (location != null) {
+            return location.getName();
+        }
+        return "";
+    }
+
     public static Timestamp convertStringToTimestamp(String time){
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
         try {
